dde1eb12494fb5cdd29e77337d4d7a3001d84326
Nouani <nouanigabriel@hotmail.com>
2020-08-04 21:22:55 -0300

    Calculo do balance na listagem

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -23,9 +23,38 @@ class TransactionsRepository {
     return this.transactions;
   }
 
-  public getBalance(): Balance {}
+  public getBalance(): Balance {
+    const income = this.transactions.reduce((totalSum, transaction): number => {
+      if (transaction.type === 'income') return totalSum + transaction.value;
 
-  public create({ title, value, type }: CreateAppointmentDTO): Transaction {}
+      return totalSum;
+    }, 0);
+
+    const outcome = this.transactions.reduce(
+      (totalSum, transaction): number => {
+        if (transaction.type === 'outcome') return totalSum + transaction.value;
+
+        return totalSum;
+      },
+      0,
+    );
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
+  }
+
+  public create({ title, value, type }: CreateAppointmentDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
 }
 
 export default TransactionsRepository;
